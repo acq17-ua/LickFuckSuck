da00f44b7aa74f5da0ae94f1fb116e2a3218c816
acq17-ua <78732643+acq17-ua@users.noreply.github.com>
2024-10-06 20:12:05 +0200

    Create conf6.bash

---
+++ conf6.bash
@@ -0,0 +1,5 @@
+./configure --prefix=/usr                      \
+            --build=$(sh support/config.guess) \
+            --host=$LFS_TGT                    \
+            --without-bash-malloc              \
+            bash_cv_strtold_broken=no
